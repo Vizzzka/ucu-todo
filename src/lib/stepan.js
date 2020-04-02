dd1a14bc8429807342d1c15fc51b3be9a7ff208c
Vizzzka <usachevavika171@gmail.com>
2020-04-02 05:01:47 +0300

    todo1

--- src/lib/stepan.js
+++ src/lib/stepan.js
@@ -1,8 +1,14 @@
+import StepanError from '/src/lib/stepanError.js';
 
 export default class Stepan {
   static createElement(element, parent, attributes = {}) {
     // TODO: check if this is a valid tag name
     const newElement = document.createElement(element);
+
+    if (newElement instanceof HTMLUnknownElement) {
+     throw new StepanError('Invalid tag name: "${element}"');
+   }
+
 
     const { innerHTML, innerText } = attributes;
 
@@ -27,6 +33,13 @@ export default class Stepan {
 
       // TODO: 1. Create StepanError class to define all framework errors
       //       2. throw an error if parent is null or undefined, or if it's not a valid DOM object
+      if (!parent) {
+        throw new StepanError('Parent is null or undefined');
+      }
+
+      if (parent instanceof HTMLUnknownElement) {
+        throw new StepanError('Parent isnt a valid DOM object');
+      }
 
       this.parent = parent;
     }
